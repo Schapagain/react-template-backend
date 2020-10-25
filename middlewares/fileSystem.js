f486fcbc7c4433f8ceadc498629e4d3384592f04
Sandesh Chapagain <sandesh@greatbear.tech>
2020-10-25 11:53:08 -0400

    add a folder that holds all user documents

---
+++ middlewares/fileSystem.js
@@ -0,0 +1,24 @@
+const { v4 : uuid } = require('uuid');
+const fs = require('fs');
+
+const id = (req,res,next) => {
+
+    console.log('creating new file system..')
+
+    // Generate unique id and inject into req
+    const uniqueId = uuid();
+    req.id = uniqueId;
+    console.log(uniqueId);
+    // Create a directory for the potential user
+    fs.mkdir('./uploads/'.concat(uniqueId),err => {
+        if (err) {
+            return res.status(500).json({
+                error: "Could not add new user. Try again later."
+            })
+        }
+    })
+    
+    next();
+}
+
+module.exports = id;
