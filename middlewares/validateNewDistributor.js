435054cdebb1a275fa202dd86c5272502dddb86f
Sandesh Chapagain <sandesh@greatbear.tech>
2020-10-25 11:53:34 -0400

    add middlewares to validate distributor before posting them

---
+++ middlewares/validateNewDistributor.js
@@ -0,0 +1,36 @@
+
+const validateNewDistributor = (req, res, next) => {
+    console.log('validating new distributor');
+    
+    const {
+        name,
+        address,
+        contact,
+        language,
+        street,
+        country,
+        state,
+        district,
+        municipality,
+        ward,
+        postal,
+        phone,
+        mobile,
+        email,
+        website,
+    } = req.body;
+
+    const allowedCountries = new Set(['nepal','usa','united states','ghana','india','norway','indonesia','china','mauritius']);
+    const allowedLanguages = new Set(['nepali','english','chinese','hindi','swahili','russian']);
+    const emailFormat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+    if (!name || !allowedCountries.has(country) || !allowedLanguages.has(language) ||!email.match(emailFormat) ) {
+        return res.status(400).json({
+            error: "Please provide all required details"
+        })
+    }
+
+    next();
+}
+
+module.exports = validateNewDistributor;
--- routes/api/distributors.js
+++ routes/api/distributors.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middlewares/auth');
+const validateNewDistributor = require('../../middlewares/validateNewDistributor');
+const prepareFilesystem = require('../../middlewares/fileSystem');
+const formParser = require('../../middlewares/formParser');
+const {postDistributor,getDistributors} = require('../services/distributors');
 
 // @route   POST api/distributors
 // @desc    Add a new distributor
 // @access  Public
-router.post('/', require('../services/distributors').postDistributor);
+router.post('/', 
+    formParser,
+    validateNewDistributor,
+    prepareFilesystem,
+    (req, res, next) => {
+        res.status(200).json({
+            message: 'you good'
+        })
+        
+    });
 
 // @route   GET api/distributors
 // @desc    View all distributors
 // @access  Private
-router.get('/', auth, require('../services/distributors').getDistributors);
+router.get('/', getDistributors);
 
 module.exports = router;
