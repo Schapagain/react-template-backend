86b9a09bd220076d9661ddc42ea6d47bdef8668c
Sandesh Chapagain <sandesh@greatbear.tech>
2020-10-25 11:48:21 -0400

    make minor adjustments

--- app.js
+++ app.js
@@ -33,12 +33,9 @@ app.use((req,res,next) => {
 
 // Handle all errors thrown
 app.use((error,req,res,next) => {
-    console.log(error.message);
     res.status(error.status || 500);
     res.json({
-        error: {
-            message: error.message,
-        }
+        error: error.message,
     });
 });
 
