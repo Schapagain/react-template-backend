b29bb6949edfe8925c2c3e4301bff707fe946a39
Sandesh Chapagain <sandesh@greatbear.tech>
2020-10-30 12:19:15 -0400

    add scope/role to jwts

--- utils/auth.js
+++ utils/auth.js
@@ -3,9 +3,10 @@ require('dotenv').config();
 const signingKey = process.env.SECRET_KEY;
 
 // This also needs to account for role of the user
-const getAuthToken = id => {
+const getAuthToken = (id,role) => {
     const claims = {
       sub: id,
+      scope: role,
     }
     const token = njwt.create(claims,signingKey);
     token.setExpiration(new Date().getTime() + (60*60*1000));
