586c5f86beee33becf4dfd25c96d543f95ca0399
Sandesh Chapagain <sandesh@greatbear.tech>
2020-10-30 09:59:27 -0400

    refactor and send better error responses

--- middlewares/validateNewDistributor.js
+++ middlewares/validateNewDistributor.js
@@ -1,4 +1,4 @@
-
+const { emailFormat } = require('../utils');
 const validateNewDistributor = (req, res, next) => {
     
     const {
@@ -21,15 +21,30 @@ const validateNewDistributor = (req, res, next) => {
 
     const allowedCountries = new Set(['nepal','usa','united states','ghana','india','norway','indonesia','china','mauritius']);
     const allowedLanguages = new Set(['nepali','english','chinese','hindi','swahili','russian']);
-    const emailFormat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    
+    if (!name || !country || !language ||!email || !documents || !(phone || mobile) || !street || !state || !postal) {
+        return sendError(res,'Please provide all required details');
+    }
 
-    if (!name || !allowedCountries.has(country) || !allowedLanguages.has(language) ||!email.match(emailFormat) || !documents || !(phone || mobile) || !street || !state || !postal) {
-        return res.status(400).json({
-            error: "Please provide all required details"
-        })
+    if (!allowedCountries.has(country)){
+        return sendError(res,'Invalid country');
+    }
+
+    if(!allowedLanguages.has(language)){
+        return sendError(res,'Invalid language');
+    }
+
+    if(!email.match(emailFormat)){
+        return sendError(res,'Invalid email');
     }
 
     next();
+}
+
+const sendError = (res,errorMessage) => {
+    return res.status(400).json({
+        error: errorMessage
+    })
 }
 
 module.exports = validateNewDistributor;
