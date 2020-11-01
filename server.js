d5bf0497d4cd5c5860e5de326791c1bb6eef5f73
Sandesh Chapagain <sandesh@greatbear.tech>
2020-11-01 18:33:10 -0500

    connected to database when server loads

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const app = require('./app');
+const db = require('./utils/db');
+
+// Test database connection
+db
+.authenticate()
+.then(() => console.log('Connected to database...'))
+.catch(err => console.error('Database connection failed: ', err.message));
 
 // Start the server and listen on port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>console.log('Listening on port',PORT));
+
+
