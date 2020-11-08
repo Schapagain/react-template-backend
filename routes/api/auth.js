fe04eeb8bd254d33fb0f271e7a6e464895fc81cf
Sandesh Chapagain <sandesh@greatbear.tech>
2020-11-07 20:26:09 -0500

    allow drivers to login only with a phone number

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,14 +5,17 @@ const bcrypt = require('bcrypt');
 const { getAuthToken } = require('../../utils/auth');
 const Login = require('../../models/Login');
 
+const { DRIVER } = require('../../utils/roles');
+const { passwordFormat } = require('../../utils');
+
 // @route   POST api/auth
 // @desc    Authenticate user
 // @access  Public
 router.post('/', async (req,res) => {
 
-    let {email,password} = req.body;
+    let { email, password, phone } = req.body;
     // Check if all fields are given
-    if (!email || !password){
+    if (!((email  && password) || phone)){
         return res.status(400).json({
             error: "Please provide all required fields"
         })
@@ -20,14 +23,28 @@ router.post('/', async (req,res) => {
 
     try{
         // Check if the user exists
-        let result = await Login.findOne({where:{email}})
+        const queryOptions = {where:email?{email}:{phone}}
+        let result = await Login.findOne(queryOptions);
         if (!result) {
             return res.status(400).json({
                 error: "Not authorized"
             })
         } 
+        
         const { id, password:passwordHash, role } = result;
+        
+        // Skip password verification in case of a driver
+        // [ TODO ] send token via text message
+        if (role === DRIVER){
+            const token = getAuthToken(id,DRIVER);
+            return res.status(200).json({
+                message: "Login successful",
+                role,
+                token
+            })
+        }
 
+        if (!passwordHash) return res.status(400).json({error:"Password has not been set."})
         // Compare username/password combination
         const credentialsMatch = await bcrypt.compare(password,passwordHash);
         if (!credentialsMatch) return res.status(401).json({error:"Unauthorized"})
