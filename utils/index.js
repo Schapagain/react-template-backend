8cc6ea77a9b7a85daa09a0c86d5cc6cbfe046b38
Sandesh Chapagain <sandesh@greatbear.tech>
2020-11-11 17:59:29 -0500

    remove formattings and add useful arrays used in validation

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,11 @@
 
 const { v4: uuid } = require('uuid');
 
-const emailFormat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-const passwordFormat = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-const expectedFiles = ['profilePicture','licenseDocument'];
+const expectedFiles = ['profilePicture','licenseDocument', 'registrationDocument'];
+
+const allowedCountries = ['nepal','usa','united states','ghana','india','norway','indonesia','china','mauritius'];
+const allowedLanguages = ['nepali','english','chinese','hindi','swahili','russian'];
 
 const getRandomId = () => uuid().slice(0,5);
 
-module.exports = { emailFormat, passwordFormat, expectedFiles, getRandomId };
+module.exports = { allowedCountries, allowedLanguages, expectedFiles, getRandomId };
