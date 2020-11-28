5920be8b7671e1b0fc52e3ced2129a45afa9fed5
Sandesh Chapagain <sandesh@greatbear.tech>
2020-11-27 20:19:02 -0500

    add a countries route

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use('/api/drivers',require('./routes/api/drivers'));
 app.use('/api/vehicles',require('./routes/api/vehicles'));
 app.use('/api/contacts',require('./routes/api/contacts'));
 app.use('/api/users',require('./routes/api/users'));
+app.use('/api/countries',require('./routes/api/countries'));
 
 // Forward invalid routes to the error handler below
 app.use((req,res,next) => {
