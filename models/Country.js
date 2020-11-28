31b2e49adde28d3c8d995dba625ab4787051cf97
Sandesh Chapagain <sandesh@greatbear.tech>
2020-11-27 20:20:22 -0500

    add name validation and distributorId field

--- models/Country.js
+++ models/Country.js
@@ -1,7 +1,18 @@
-
+const allCountries = require('iso3166-2-db');
+const { ValidationError, NotUniqueError } = require('../utils/errors');
 
 module.exports = function(sequelize, DataTypes) {
     const Country = sequelize.define('Country', {
+        distributorId: {
+            type: DataTypes.INTEGER,
+            field: 'distributor_id',
+            validate: {
+                isNotSuperuser(id){
+                    if (Number(id) <= 1)
+                        throw new ValidationError('distributorId','must be greater than 1');
+                }
+            }
+        },
         id: {
             type: DataTypes.INTEGER,
             autoIncrement: true,
@@ -10,6 +21,19 @@ module.exports = function(sequelize, DataTypes) {
         name: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                isValidCountry(name){
+                    if (!allCountries.findCountryByName(name))
+                        throw new ValidationError('country name','does not exist on Earth');
+                },
+                async isUniqueCountry(name){
+                    const country = await Country.findOne({where:{distributorId: this.distributorId,name}});
+                    if (country){
+                        console.log(country)
+                        throw new NotUniqueError('country with that name');
+                    }
+                }
+            }
         },
         createdAt: {
             type: DataTypes.DATE,
