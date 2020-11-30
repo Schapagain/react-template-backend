bb612bb938d745eed6c5d2e87eb34d1af63bf20e
Sandesh Chapagain <sandesh@greatbear.tech>
2020-11-30 17:38:19 -0500

    add migrations for packages

---
+++ migrations/20201130220336-create-packages.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('packages',{
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+      },
+      price: {
+          type: Sequelize.FLOAT,
+          allowNull: false,
+      },
+      duration: {
+          type: Sequelize.STRING,
+          allowNull: false,
+      },
+      createdAt: {
+          type: Sequelize.DATE,
+          field: 'created_at'
+      },
+      updatedAt: {
+          type: Sequelize.DATE,
+          field: 'updated_at'
+      },
+      deletedAt: {
+          type: Sequelize.DATE,
+          field: 'deleted_at'
+      }
+  })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('packages');
+  }
+};
---
+++ migrations/20201130220453-add-distributor-package-association.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'packages',
+      'distributor_id',
+      {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'distributors',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(
+      'packages',
+      'distributor_id'
+    )
+  }
+};
