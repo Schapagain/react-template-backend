4c590616a5272ccb6a05bafd0b2d377f3c2acb81
Sandesh Chapagain <sandesh@greatbear.tech>
2020-12-03 15:00:30 -0500

    add functions for CRUD

--- services/countries.js
+++ services/countries.js
@@ -1,76 +1,73 @@
 
-const auth = require('../middlewares/auth');
-const path = require('path');
-const { getRandomId } = require('../utils');
-const fs = require('fs');
-const { Login, Driver, Distributor, sequelize, Vehicle, Country} = require('../models');
-const { DRIVER } = require('../utils/roles');
+const { Distributor, Country, State} = require('../models');
 const { getError, NotAuthorizedError, ValidationError, NotFoundError } = require('../utils/errors');
-const { expectedFiles } = require('../utils')
 
 async function postCountry(country) {
     try{
-        const { distributorId } = country;
-        if (!distributorId)
-            throw new ValidationError('distributorId');
-
-        const distributor = await Distributor.findOne({where:{id:distributorId}});
-        if (!distributor)
-            throw new NotAuthorizedError(' distributor with that token/id does not exist!'); 
-
-        country = await distributor.createCountry(country);
+        // remove if id given
+        delete country.id;
+        
+        country = await Country.create(country);
         return country.dataValues;
     }catch(err){
         throw await getError(err);
     }
 }
 
-async function getCountries(distributorId) {
+async function getCountries(id) {
     try {
+        if (id && isNaN(id))
+            throw new ValidationError('id parameter');
 
-        if (!distributorId)
-            throw new ValidationError('distributor Id');
-
-        const distributor = await Distributor.findOne({where:{id:distributorId}});
-
-        if (!distributor)
-            throw new NotAuthorizedError('distributor with that token does not exist');
-        let allCountries;        
-        if (distributor.isSuperuser){
-            allCountries = await Country.findAll();
-        }else{
-            allCountries = await distributor.getCountries();
-        }
-
+        const query = id? {id} : {};
+        allCountries = await Country.findAll({where:query,include:State});
+        allCountries = allCountries.map(country => {
+            const {id, name, States} = country;
+            return {
+                id,name,
+                States: States.map(state => {
+                    const { id, name } = state;
+                    return { id, name }
+                })
+            }
+        })
         return {count: allCountries.length, data: allCountries}
-
     }catch(err){
         throw await getError(err);
     }
 }
 
-async function deleteCountry(distributorId,id) {
+async function updateCountry(country){
+    try{
+        const { id } = country;
+
+        if (isNaN(id))
+            throw new ValidationError('id parameter');
+        oldCountry = await Country.findOne({where:{id}});
+        if (!oldCountry)
+            throw new NotFoundError('country');
+        Country.update(country,{where:{id:oldCountry.id}});
+        return country;
+    }catch(err){
+        throw await getError(err);
+    }
+}
+
+async function deleteCountry(id) {
 
     try{
-        const distributor = await Distributor.findOne({where:{id:distributorId}});
-        if (!distributor)
-            throw new NotAuthorizedError('distributor with that token does not exist'); 
-
-        let country;        
-        if (distributor.isSuperuser){
-            country = await Country.findOne({where:{id}});
-        }else{
-            country = await distributor.getCountries({where:{id}});
-        } 
+        if (isNaN(id))
+            throw ValidationError('id parameter')
+            
+        country = await Country.findOne({where:{id}});
         if (!country)
             throw new NotFoundError('country')
-
         Country.destroy({where:{id}});
-        return country.dataValues;
+        return {id: country.id,name:country.name};
     }catch(err){
         throw await getError(err);
     }
     
 }
 
-module.exports = { postCountry, getCountries, deleteCountry };
+module.exports = { postCountry, getCountries, updateCountry, deleteCountry };
---
+++ services/localities.js
@@ -0,0 +1,86 @@
+
+const { Locality, State, Ward, Municipality} = require('../models');
+const { getError, ValidationError, NotFoundError } = require('../utils/errors');
+
+async function postLocality(locality) {
+    try{
+        const { municipalityId } = locality;
+        if (!municipalityId || isNaN(municipalityId))
+            throw new ValidationError('municipalityId','not found');
+
+        const municipality = await Municipality.findOne({where: {id: municipalityId}});
+        if (!municipality)
+            throw new NotFoundError('municipality')
+
+        // Set localities countryId,stateId, and districtId the same as that of municipality's
+        locality = {
+            ...locality,
+            countryId: municipality.countryId,
+            stateId: municipality.stateId,
+            districtId: municipality.districtId,
+        }
+        // remove id if given
+        delete locality.id
+        
+        locality = await municipality.createLocality(locality);
+        return {id: locality.id, name: locality.name}
+    }catch(err){
+        throw await getError(err);
+    }
+}
+
+async function getLocalities(municipalityId) {
+    try {
+        if (municipalityId) {
+            const municipality = await Municipality.findOne({where:{id:municipalityId}});
+            if (!municipality)
+                throw new NotFoundError('municipality');
+        }
+        const query = municipalityId? {municipalityId} : {};
+        const allLocalities = await Locality.findAll({where:query,include: Ward});      
+        return {count: allLocalities.length, data: allLocalities.map(locality=>{
+            const {municipalityId,id,name,Wards} = locality;
+            return {
+                municipalityId,
+                id,
+                name,
+                wards: Wards? Wards.map(ward => ({id: ward.id, number: ward.number})) : []
+            }})
+        }
+    }catch(err){
+        throw await getError(err);
+    }
+}
+
+
+async function updateLocality(locality) {
+
+    try{
+        const { id } = locality
+        oldLocality = await Locality.findOne({where:{id}});
+        if (!oldLocality)
+            throw new NotFoundError('locality')
+        Locality.update(locality,{where:{id}});
+        return locality;
+    }catch(err){
+        throw await getError(err);
+    }
+    
+}
+
+async function deleteLocality(id) {
+
+    try{
+        const locality = await Locality.findOne({where:{id}});
+        if (!locality)
+            throw new NotFoundError('locality')
+
+        Locality.destroy({where:{id}});
+        return locality.dataValues;
+    }catch(err){
+        throw await getError(err);
+    }
+    
+}
+
+module.exports = { postLocality, getLocalities, updateLocality, deleteLocality };
