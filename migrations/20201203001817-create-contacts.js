0af50a7aa6e3814899690703004f4caa9257dabb
Sandesh Chapagain <sandesh@greatbear.tech>
2020-12-02 19:26:29 -0500

    create contacts table

---
+++ migrations/20201203001817-create-contacts.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('contacts',{
+      distributorId: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          field: 'distributor_id',
+          foreignKey: true,
+      },
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+      },
+      jobPosition: {
+          type: Sequelize.STRING,
+          field: 'job_position',
+      },
+      title: Sequelize.STRING,
+      email: Sequelize.STRING,
+      phone: Sequelize.STRING,
+      mobile: Sequelize.STRING,
+      createdAt: {
+          type: Sequelize.DATE,
+          field: 'created_at'
+      },
+      updatedAt: {
+          type: Sequelize.DATE,
+          field: 'updated_at'
+      },
+      deletedAt: {
+          type: Sequelize.DATE,
+          field: 'deleted_at'
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('contacts');
+  }
+};
