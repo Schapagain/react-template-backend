aa9d492e4fb8ff03d85a4b4071fcbc4e6c5ddbd3
Sandesh Chapagain <sandesh@greatbear.tech>
2020-12-19 11:21:01 -0500

    make package duration an integer

--- models/Package.js
+++ models/Package.js
@@ -26,7 +26,7 @@ module.exports = function(sequelize, DataTypes) {
             allowNull: false,
         },
         duration: {
-            type: DataTypes.STRING,
+            type: DataTypes.INTEGER,
             allowNull: false,
         },
         createdAt: {
