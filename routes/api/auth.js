76ff020feb6277e7fb2855dccf6aea69d61d59c1
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-09 17:05:39 -0500

    fix imports

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const { getAuthToken } = require('../../utils/auth');
+const { getAuthToken } = require('../../services/auth');
 const { getRoles } = require('../../utils');
 const { Login } = require('../../models');
 const formParser = require('../../middlewares/formParser');
