107fda63da8cd83fd6a0dd25d05e2b349853bb88
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-09 17:02:15 -0500

    refactor

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,74 +3,96 @@ require('dotenv').config();
 const signingKey = process.env.SECRET_KEY;
 const { getError, ValidationError, NotAuthorizedError } = require('../utils/errors');
 
-const { Distributor, Driver, User, Vehicle, Country, Package, Contact, Subscription } = require('../models');
-const { ADMIN, DISTRIBUTOR, DRIVER } = require('../utils/roles');
+const { ADMIN } = require('../utils/roles');
+const { getValidAuthMethods, getRoutePermissions } = require('../../services/auth');
 
-// Acess controls based on roles remains to be implemented here
+/**
+ * Verify that the role and id associated with the token 
+ * is allowed to access the attempted route
+ */
 const auth = async (req,res,next) => {
-
-    const modelMap = {
-        'drivers' : [Driver,new Set([ADMIN,DISTRIBUTOR])],
-        'distributors' : [Distributor,new Set([ADMIN,DISTRIBUTOR])],
-        'users' : [User,new Set([ADMIN,DISTRIBUTOR])],
-        'vehicles' : [Vehicle,new Set([ADMIN,DRIVER,DISTRIBUTOR])],
-        'countries' : [Country,new Set([ADMIN])],
-        'states' : [Country,new Set([ADMIN])],
-        'districts' : [Country,new Set([ADMIN])],
-        'municipalities' : [Country,new Set([ADMIN])],
-        'localities' : [Country,new Set([ADMIN])],
-        'wards' : [Country,new Set([ADMIN])],
-        'packages' : [Package,new Set([ADMIN,DISTRIBUTOR])],
-        'contacts' : [Contact, new Set([ADMIN,DISTRIBUTOR])],
-        'subscriptions':[Subscription, new Set([ADMIN,DISTRIBUTOR])],
-    }
 
     try{
         // Get token from the header
-        const userToken = req.header('authorization') || req.params.token;
-        if (!userToken) return res.status(400).json({error:"No token found"})
-
-        // Verify token and extract user id
-        const token = njwt.verify(userToken,signingKey);
-        const tokenId = token.body.sub;
-        const tokenRole = token.body.scope;
-
+        const token = parseAuthHeader(req.header('authorization'));
+        const {id,role} = parseToken(token,signingKey);
+        await verifyRouteOwnership(req.baseUrl,req.params.id,id,role);
+        
         // Inject userId into req before proceeding
-        req.auth = {
-            id: tokenId,
-            role: tokenRole
-        }
-
-        // [LOG] 
-        console.log('Request using token with id:',tokenId,'role:',tokenRole)
-
-        // Check route permissions
-        const apiBaseString = req.baseUrl.split('/').pop();
-        const model = modelMap[apiBaseString][0];
-        const allowedRoles = modelMap[apiBaseString][1]
-        if (!allowedRoles.has(tokenRole))
-            throw new NotAuthorizedError('Restricted route');
-
-        // Allow admins unrestricted access
-        // If an id param is passed and the user is accessing their own data, allow access
-        // if not, check if the user is accessing their childrens' data
-        // If neither, throw unauthorized
-        if (tokenRole != ADMIN && req.params.id && req.params.id != tokenId){
-            if (isNaN(req.params.id))
-                throw new ValidationError('id parameter')
-
-            let result;
-            if (apiBaseString === 'distributors'){
-                result = await model.findOne({where:{parentId:tokenId,id:Number(req.params.id)}})
-            }else{
-                result = await model.findOne({where:{distributorId:tokenId,id:Number(req.params.id)}}) 
-            }
-            if (!result) 
-                throw new NotAuthorizedError('Private route');
-        }
+        req.auth = {id,role};
         next();
     }catch(err){
         next(await getError(err))
+    }
+}
+
+
+/**
+ * Extracts user token from Authorization header
+ * @param {String} authHeader 
+ */
+function parseAuthHeader(authHeader) {
+
+    const [authMethod,userToken] = authHeader
+    ? authHeader.split(' ') 
+    : ['',''];
+
+    // ensure existence of auth method and token in correct format
+    if (!userToken && !authMethod) throw new ValidationError('token')
+    const methodIsValid = getValidAuthMethods().includes(authMethod.toLowerCase());
+    if (!authMethod || !methodIsValid) throw new ValidationError('token','auth format not supported');
+    if (!userToken) throw new ValidationError('token');
+
+    return userToken;
+}
+
+/**
+ * Verify the given token, and return owner's id, role
+ * @param {String} userToken 
+ * @param {String} signingKey 
+ */
+function parseToken(userToken,signingKey) {
+    const token = njwt.verify(userToken,signingKey);
+    const id = token.body.sub;
+    const role = token.body.scope;
+    return {id,role}
+}
+
+/**
+ * Ensure that users can only access their own, or their childrens resources 
+ * @param {String} baseUrl
+ * @param {String} ownerId 
+ * @param {String} id 
+ * @param {String} role
+ */
+async function verifyRouteOwnership(baseUrl,ownerId,id,role) {
+
+    // Allow admins unrestricted access
+    if (role === ADMIN) return;
+
+    // Check route permissions
+    const modelMap = getRoutePermissions();
+    const apiBaseString = baseUrl.split('/').pop();
+    const model = modelMap[apiBaseString][0];
+    const allowedRoles = modelMap[apiBaseString][1]
+    if (!allowedRoles.has(role))
+        throw new NotAuthorizedError('Restricted route');
+
+    // If an id param is passed and the user is accessing their own data, allow access
+    // if not, check if the user is accessing their childrens' data
+    // If neither, throw unauthorized
+    if  (ownerId && ownerId != id){
+        if (isNaN(ownerId))
+            throw new ValidationError('id parameter')
+
+        let result;
+        if (apiBaseString === 'distributors'){
+            result = await model.findOne({where:{parentId:id,id:Number(ownerId)}})
+        }else{
+            result = await model.findOne({where:{distributorId:id,id:Number(ownerId)}}) 
+        }
+        if (!result) 
+            throw new NotAuthorizedError('Private route');
     }
 }
 
