936aee0ae3eb44be6c4e3daed88948df8aac0314
Sandesh Chapagain <sandesh@greatbear.tech>
2021-01-11 10:21:19 -0500

    add route to calculate distance given two coordinates

--- routes/api/trips.js
+++ routes/api/trips.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middlewares/auth');
-const formParser = require('../../middlewares/formParser');
-const { calculateFare } = require('../../controllers/trips');
+const { calculateFare, calculateDistance } = require('../../controllers/trips');
 
 /**
- * Route to add a new ward
- * @name    api/wards
+ * Route to calculate fare given distance and wait time
+ * @name    api/trips/calculate_fare
  * @method  POST
  * @access  Admin
  * @inner
  * @param   {string} path
- * @param   {callback} middleware - Authenticate
- * @param   {callback} middleware - Form Parser  
  * @param   {callback} middleware - Handle HTTP response
 */
 router.post('/calculate_fare',
-    formParser,
     async (req, res) => {
         try{
             let result = await calculateFare(req.body);
@@ -30,6 +25,27 @@ router.post('/calculate_fare',
         }
     });
 
-
+/**
+ * Route to calculate distance given origin and destination coordinates
+ * @name    api/trips/calculate_distance
+ * @method  POST
+ * @access  Admin
+ * @inner
+ * @param   {string} path
+ * @param   {callback} middleware - Handle HTTP response
+*/
+router.post('/calculate_distance',
+    async (req, res) => {
+        try{
+            let result = await calculateDistance(req.body);
+            res.status(200).json(result);
+        }catch(err){
+            res.status(err.httpCode || 500).json({ error : {
+                field: err.field,
+                msg: err.message
+                }
+            })
+        }
+    });
 
 module.exports = router;
