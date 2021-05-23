dc54f3e60be8a544e929e6de6a391bac2faed05e
Sandesh <sandesh@greatbear.tech>
2021-05-22 20:39:38 -0400

    remove socketio code

--- server.js
+++ server.js
@@ -1,33 +1,12 @@
 const app = require("./app");
 const { sequelize } = require("./database/models");
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
 
 // Test database connection
 sequelize
   .authenticate()
   .then(() => console.log("Connected to database..."))
   .catch((err) => console.error("Database connection failed: ", err.message));
-
-// [TODO] Socket stuff to refactor later
-io.on("connection", (socket) => {
-  console.log("connected to socket", socket.id);
-
-  socket.on("post-location", ({ x, y, timeStamp }) => {
-    console.log("got a new location", x, y);
-
-    const driverName = "Tim";
-    const licensePlate = "NYV4ST8";
-
-    socket.broadcast.emit("update-location", {
-      driverName,
-      licensePlate,
-      x,
-      y,
-      timeStamp,
-    });
-  });
-});
 
 // Start the server and listen on port
 const PORT = process.env.PORT || 5000;
